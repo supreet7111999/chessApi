4e5ce8a953ba5542fa3595be5c8bb63ab89c0b17
Supreet <supreet7111999@gmail.com>
2025-07-25 20:37:07 +0530

    Rectified bugs in socket

--- src/socket.js
+++ src/socket.js
@@ -1,24 +1,23 @@
-const socket = require("socket.io");
-// const cors=require("cors");
+const { Server } = require("socket.io");
 
 const roomPlayers = {};
 
-const initializeSocket = (server) => 
-    {
-  const io = socket(server, 
-    {
+const initializeSocket = (server) => {
+  const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173/",
-    },
-  })
-  io.on("connection",(socket)=>{
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
+  io.on("connection", (socket) => {
     console.log("Connected hogye");
-    socket.on("churan",()=>{
-      console.log("chuan");
-    })
-  })
-}
 
+    socket.on("churan", () => {
+      console.log("chuan"); // ✅ This should print
+    });
+  });
+};
 
-
-module.exports=initializeSocket;
+module.exports = initializeSocket;
